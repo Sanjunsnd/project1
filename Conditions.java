194fb0318491b2f7515c03caeb5654f1ac9afb5a
sanjunsnd <sanjukumarnd@gmail.com>
2022-10-08 19:40:18 +0530

    First commit

---
+++ Conditions.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Conditions {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age > 18) {
+            System.out.println("Adult");
+        } else {
+            system.out.println("Not Adult");
+        }
+    }
+}
